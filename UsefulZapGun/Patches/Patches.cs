186a4513768705d75b612668dd822232b999ad6b
xntkrnl <maminbors@gmail.com>
2024-12-01 18:50:48 +0300

    0.0.0 ig

--- UsefulZapGun/Patches/Patches.cs
+++ UsefulZapGun/Patches/Patches.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
@@ -8,9 +9,14 @@ namespace UsefulZapGun.Patches
 {
     internal class UZGPatches
     {
+        static bool alreadyPatched = false;
+
         [HarmonyPostfix, HarmonyPatch(typeof(MenuManager), "Start")]
         static void MenuManagerStartPatch()
         {
+            if (alreadyPatched)
+                return;
+
             var enemyArray = Resources.FindObjectsOfTypeAll<EnemyType>(); //thanks, Zaggy1024
             foreach (EnemyType enemy in enemyArray)
             {
@@ -24,8 +30,37 @@ namespace UsefulZapGun.Patches
                     Plugin.SpamLog($"{enemy.enemyName} can be stunned now!", Plugin.spamType.info);
                 }
             }
+
+            alreadyPatched = true; //exiting to the menu launches this too, so we prevent the search just in case
         }
 
+        [HarmonyPostfix, HarmonyPatch(typeof(EnemyAICollisionDetect), "IShockableWithGun.ShockWithGun")]
+        static void ShockWithGunPatch(ref EnemyAI ___mainScript, ref EnemyAICollisionDetect __instance)
+        {
+            if (___mainScript.gameObject.name == "RedLocustBees(Clone)")
+            {
+                StartOfRound.Instance.StartCoroutine(WaitAndDoSmth(___mainScript, __instance));
+            }
+        }
 
+        private static IEnumerator WaitAndDoSmth(EnemyAI beeScript, EnemyAICollisionDetect instance)
+        {
+            yield return new WaitForSeconds(3f);
+            
+            if (beeScript.stunNormalizedTimer > 0f)
+            {
+                var allZapGuns = GameObject.FindObjectsOfType<PatcherTool>(); //uuh i don't feel comfortable to do it at runtime
+                foreach (PatcherTool zapgun in allZapGuns)
+                {
+                    if (zapgun.isBeingUsed && zapgun.shockedTargetScript == instance)
+                    {
+                        zapgun.StopShockingAnomalyOnClient(true);
+                        break;
+                    }
+                }
+                Landmine.SpawnExplosion(beeScript.gameObject.transform.position, true, 0, 3, 20, 3);
+                StartOfRound.Destroy(beeScript.gameObject);
+            }
+        }
     }
 }
