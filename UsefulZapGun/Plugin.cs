4d0a4caf6c42ebc199dc9b8ccbb95f960f5cb363
xntkrnl <maminbors@gmail.com>
2024-12-01 21:44:57 +0300

    forgot to add netcode patcher ><

--- UsefulZapGun/Plugin.cs
+++ UsefulZapGun/Plugin.cs
@@ -3,6 +3,8 @@ using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using System.IO;
+using System.Reflection;
+using UnityEngine;
 using UsefulZapGun.Patches;
 
 namespace UsefulZapGun
@@ -19,8 +21,27 @@ namespace UsefulZapGun
         private static ManualLogSource mls;
         private static Plugin Instance;
 
+        private static void NetcodePatcher()
+        {
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (var type in types)
+            {
+                var methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+                foreach (var method in methods)
+                {
+                    var attributes = method.GetCustomAttributes(typeof(RuntimeInitializeOnLoadMethodAttribute), false);
+                    if (attributes.Length > 0)
+                    {
+                        method.Invoke(null, null);
+                    }
+                }
+            }
+        }
+
         void Awake()
         {
+            NetcodePatcher();
+
             Instance = this;
             mls = BepInEx.Logging.Logger.CreateLogSource(modName);
             mls = Logger;
