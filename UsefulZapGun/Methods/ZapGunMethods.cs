466a4d2e82a0f4d522b9109ebcc38225fe39eeaf
xntkrnl <maminbors@gmail.com>
2024-12-06 21:37:31 +0300

    Old functionality

--- UsefulZapGun/Methods/ZapGunMethods.cs
+++ UsefulZapGun/Methods/ZapGunMethods.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Text;
 using Unity.Netcode;
 using UnityEngine;
 using UsefulZapGun.Patches;
@@ -10,13 +8,24 @@ namespace UsefulZapGun.Methods
 {
     internal class ZapGunMethods
     {
+        internal static List<PatcherTool> zapGuns = new List<PatcherTool>();
+
         internal static IEnumerator WaitAndDoSmth(EnemyAI enemyScript, EnemyAICollisionDetect instance)
         {
             yield return new WaitForSeconds(3f);
 
             if (enemyScript.stunNormalizedTimer > 0f)
             {
-
+                foreach (PatcherTool tool in zapGuns)
+                {
+                    if (tool.isBeingUsed && tool.playerHeldBy == GameNetworkManager.Instance.localPlayerController)
+                    {
+                        NetworkObjectReference enemyNOR = new NetworkObjectReference(enemyScript.gameObject.GetComponent<NetworkObject>());
+                        tool.StopShockingAnomalyOnClient();
+                        //do smth
+                        GameNetworkManagerPatch.hostNetHandler.DestroyEnemyServerRpc(enemyNOR);
+                    }
+                }
             }
         }
     }
--- UsefulZapGun/Patches/PatcherToolPatch.cs
+++ UsefulZapGun/Patches/PatcherToolPatch.cs
@@ -12,6 +12,23 @@ namespace UsefulZapGun.Patches
 {
     internal class PatcherToolPatch
     {
+        //[HarmonyPostfix, HarmonyPatch(typeof(PatcherTool), "Start")]
+
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PatcherTool), "BeginShockingAnomalyOnClient")]
+        static void AddZapGun(ref PatcherTool __instance)
+        {
+            ZapGunMethods.zapGuns.Add(__instance);
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PatcherTool), "StopShockingAnomalyOnClient")]
+        static void RemoveZapGun(ref PatcherTool __instance)
+        {
+            if (!ZapGunMethods.zapGuns.Remove(__instance))
+            {
+                Plugin.SpamLog($"Couldn't remove the zap gun! networkObjectID: {__instance.NetworkObjectId}", Plugin.spamType.error);
+            }
+        }
 
     }
 }
