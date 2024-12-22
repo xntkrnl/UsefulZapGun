e6803763fe1f166a63ff96a7f42fd301574745d4
xntkrnl <maminbors@gmail.com>
2024-12-22 12:39:06 +0300

    0.3.1

--- UsefulZapGun/Plugin.cs
+++ UsefulZapGun/Plugin.cs
@@ -16,7 +16,7 @@ namespace UsefulZapGun
         // Mod Details
         private const string modGUID = "mborsh.UsefulZapGun";
         private const string modName = "UsefulZapGun";
-        private const string modVersion = "0.3.0";
+        private const string modVersion = "0.3.1";
 
         private readonly Harmony harmony = new Harmony(modGUID);
         private static ManualLogSource mls;
